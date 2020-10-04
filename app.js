ed629a4a2d789056d5a70ca9f1057f8eddc7c572
florian markusse <f.j.markusse@student.tue.nl>
2020-10-04 20:26:10 +0200

    created batch mode

--- app.js
+++ app.js
@@ -10,37 +10,49 @@ import { clearFixSet } from './fixes/fix.mjs';
 import { addToUnfixableSet } from './fixes/fix.mjs';
 import { containsInUnfixableSet } from './fixes/fix.mjs';
 
-const fileString = './test_files/' + String(process.argv[2]);
+let fileStringsToCheck = [];
 
-fs.readFile(fileString, function read(err, data) {
-    if (err) {
-        throw err;
-    }
+if (String(process.argv[2]).includes(".js") || String(process.argv[2]).includes(".mjs")) { 
+    fileStringsToCheck[0] = './test_files/' + String(process.argv[2]);
+} else {
+    // Batch mode
+    let result = getAllFilesRecursively(String(process.argv[2]));
+    fileStringsToCheck = getOnlyJSFiles(result);
+    console.log(fileStringsToCheck);
+}
 
-    let ast = espree.parse(data, { tokens: false, ecmaVersion: 11 });
-    
-    let previousFix;
-    let fixToDo;
-    do {
-        processAST(ast);
-        let iterator = getFixSet().values();
-        fixToDo = iterator.next().value;
-
-        while ((previousFix !== undefined && fixToDo !== undefined && fixToDo.isEqualTo(previousFix)) || containsInUnfixableSet(fixToDo)) {
-            addToUnfixableSet(fixToDo);
-            fixToDo = iterator.next().value;
-        } 
-
-
-        if (fixToDo !== undefined) {
-            fixToDo.fix(ast);
-            ast = espree.parse(escodegen.generate(ast), { tokens: false, ecmaVersion: 11 });
-            previousFix = fixToDo;
+fileStringsToCheck.forEach(fileString => {
+    fs.readFile(fileString, function read(err, data) {
+        if (err) {
+            throw err;
         }
-    } while(fixToDo !== undefined/*false*/);
+    
+        let ast = espree.parse(data, { tokens: false, ecmaVersion: 11 });
+        
+        let previousFix;
+        let fixToDo;
+        do {
+            processAST(ast);
+            let iterator = getFixSet().values();
+            fixToDo = iterator.next().value;
+    
+            while ((previousFix !== undefined && fixToDo !== undefined && fixToDo.isEqualTo(previousFix)) || containsInUnfixableSet(fixToDo)) {
+                addToUnfixableSet(fixToDo);
+                fixToDo = iterator.next().value;
+            } 
+    
+    
+            if (fixToDo !== undefined) {
+                fixToDo.fix(ast);
+                ast = espree.parse(escodegen.generate(ast), { tokens: false, ecmaVersion: 11 });
+                previousFix = fixToDo;
+            }
+        } while(fixToDo !== undefined/*false*/);
+    
+        console.log(escodegen.generate(ast));
+    });
+})
 
-    console.log(escodegen.generate(ast));
-});
 
 function processAST(ast) {
 
@@ -65,6 +77,52 @@ function processAST(ast) {
 function clearGlobals() {
     clearVariablesMap();
     clearFixSet();
+}
+
+function getAllFilesRecursively(fileName) {
+
+    let fileNames = fs.readdirSync(fileName);
+
+    for (let i = 0; i < fileNames.length; i++) {
+        if (!fileNames[i].includes(".")) {
+            fileNames[i] = getAllFilesRecursively(fileName + "\\" + fileNames[i])
+            i += fileNames[i].length - 1;
+            fileNames = flatten(fileNames);
+        } else {
+            fileNames[i] = fileName + "\\" + fileNames[i];
+        }
+    }
+
+    return fileNames;
+}
+
+function flatten(input) {
+    const stack = [...input];
+    const res = [];
+    while(stack.length) {
+      // pop value from stack
+      const next = stack.pop();
+      if(Array.isArray(next)) {
+        // push back array items, won't modify the original input
+        stack.push(...next);
+      } else {
+        res.push(next);
+      }
+    }
+    // reverse to restore input order
+    return res.reverse();
+  }
+
+function getOnlyJSFiles(files) {
+    let jsFiles = [];
+
+    files.forEach(file => {
+        if (file.includes(".js") || file.includes(".mjs")) {
+            jsFiles.push(file);
+        }
+    })    
+    
+    return jsFiles;
 }
 
 
