4229a3b61f0aa5edbf810ebe4dc3cd7dcce0e8f4
Florian Markusse <florianmarkusse@hotmail.com>
2020-11-17 20:32:17 +0100

    dfd

---
+++ firstAction.cjs
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const { graphql } = require("@octokit/graphql");
+const { Octokit } = require("@octokit/rest");
+
+try {
+  console.log("testing");
+
+  const octokit = new Octokit({
+    auth: `token ${repoToken}`,
+    userAgent: 'Branch Protection script',
+    baseUrl: `https://api.${CONST.GITHUB_URL}`,
+    log: {
+      debug: () => {
+      },
+      info: () => {
+      },
+      warn: console.warn,
+      error: console.error
+    },
+    previews: ['antiope-preview']
+  });
+
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${repoToken}`
+    }
+  });
+  const { context } = github;
+  const { owner, repo } = context.repo;
+
+  console.log(owner);
+  console.log(repo);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
