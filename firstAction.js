e1c358305d3d78afecdfb771677b50f3c2e636c8
Florian Markusse <florianmarkusse@hotmail.com>
2020-11-17 19:48:50 +0100

    changed action

--- firstAction.js
+++ firstAction.js
@@ -1,15 +1,49 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/core';
+import Octokit from '@octokit/rest';
+import { graphql } from '@octokit/graphql';
+
+const repoToken = core.getInput('repo-token', { required: true });
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+    const octokit = new Octokit({
+        auth: `token ${repoToken}`,
+        userAgent: 'Branch Protection script',
+        baseUrl: `https://api.github.com`,
+        log: {
+          debug: () => {
+          },
+          info: () => {
+          },
+          warn: console.warn,
+          error: console.error
+        },
+        previews: ['antiope-preview']
+      });
+
+    const graphqlWithAuth = graphql.defaults({
+    headers: {
+        authorization: `token ${repoToken}`
+    }
+    });
+
+    const { context } = github;
+    const { owner, repo } = context.repo;
+
+    const prInfo = await getPullRequestInfo({
+        graphqlWithAuth,
+        prNumber: context.issue.number,
+        owner,
+        repo
+    });
+
+    console.warn(prInfo.repository.pullRequest.files.nodes);
+
+} catch(error) {
+    console.error('Error', err.stack);
+    if (err.data) {
+        console.error(err.data);
+    }
+    core.setFailed(err.message);
 }
