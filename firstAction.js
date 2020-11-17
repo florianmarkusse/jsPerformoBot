31cbd4420a3d851fefcb9610b38518da25ea4de4
Florian Markusse <florianmarkusse@hotmail.com>
2020-11-17 20:03:26 +0100

    testing

--- firstAction.js
+++ firstAction.js
@@ -1,15 +1,13 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import Octokit from '@octokit/rest';
+import * as github from '@actions/github';
+import { graphql } from '@octokit/graphql';
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log("testing");
+
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
