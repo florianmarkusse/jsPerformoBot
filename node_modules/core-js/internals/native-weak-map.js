5ef8448c48deb41836556d324f4ceddf82ff2afa
florianmarkusse <florianmarkusse@hotmail.com>
2022-01-23 17:48:22 +0100

    beep

--- node_modules/core-js/internals/native-weak-map.js
+++
@@ -1,6 +0,0 @@
-var global = require('./global');
-var inspectSource = require('./inspect-source');
-
-var WeakMap = global.WeakMap;
-
-module.exports = typeof WeakMap === 'function' && /native code/.test(inspectSource(WeakMap));
